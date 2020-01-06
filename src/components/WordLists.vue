d5b4ea52edaff5d4292f5d0cf8e0beafba7f70d4
aleksei <a_makarov_alex@mail.ru>
2020-01-06 14:41:12 +0300

    first commit. Test method works on localhost

---
+++ src/components/WordLists.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="meanings_list">
+    <h1>Meanings available</h1>
+    <ul
+      v-for="meaning in meanings"
+      :key="meaning.title"
+      class="meanings"
+    >
+      <li class="meaning-item">
+        <h3>{{ meaning.definition }}</h3>
+      </li>
+    </ul>
+    <div>
+      <h1>{{ this.$store.state.language }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    computed: {
+      meanings() {
+        return this.$store.state.meanings
+      }
+    },
+    created() {
+      this.$store.dispatch('loadLanguage')
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {API_URL} from '../../api/config'
+import axios from 'axios'
+import VueAxios from "vue-axios";
+
+Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+Vue.axios.defaults.baseURL = API_URL
+Vue.axios.defaults.headers.get['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+
+export const store = new Vuex.Store({
+  state: {
+    language: '',
+    meanings: [
+      {definition: 'def1'},
+      {definition: 'def2'},
+      {definition: 'def3'}
+    ]
+  },
+  mutations: {
+    SAVE_LANGUAGE (state, language) {
+      state.language = language
+    }
+  },
+  actions: {
+    loadLanguage({commit}) {
+      Vue.axios.get('language').then(result => {
+        commit('SAVE_LANGUAGE', result.data)
+      }).catch(error => {
+        throw new Error('API ${error}')
+      })
+    }
+  }
+})
