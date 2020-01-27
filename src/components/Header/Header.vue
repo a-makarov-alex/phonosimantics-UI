d5eb81ad811a2408a8f7870e40f7f391f63e870d
aleksei <a_makarov_alex@mail.ru>
2020-01-28 01:32:26 +0300

    basic select

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="header-line">
-      <h2>Phonosemantics-UI</h2>
-    </div>
+  <div class="header-line">
+    <h2>Phonosemantics-UI</h2>
+    <select name="meaning" id="">
+      <option disabled value="">Select meaning</option>
+      <option v-for="meaning in meanings">{{meaning}}</option>
+    </select>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Header"
+  export default {
+    name: "Header",
+    computed: {
+      meanings() {
+        return this.$store.state.meanings
+      }
+    },
+    created() {
+      this.$store.dispatch('loadMeanings')
     }
+  }
 </script>
 
 <style scoped>
@@ -20,6 +32,13 @@
 
   h2 {
     margin-left: 5%;
+    margin-bottom: auto;
+    margin-top: auto;
+  }
+
+  select {
+    margin-left: auto;
+    margin-right: 10%;
     margin-bottom: auto;
     margin-top: auto;
   }
